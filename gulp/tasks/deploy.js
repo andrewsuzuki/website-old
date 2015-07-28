1857ed74d20ce3654832ebf0a59d0e139d238d4d
Andrew Suzuki <andrew.b.suzuki@gmail.com>
2015-07-28 00:17:25 -0400

    fix deploy, and http->https for fonts

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -2,7 +2,8 @@
 
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
-var path = require('path')
+var path = require('path');
+var config = require('../config');
 
 gulp.task('deploy', ['prod'], function() {
     return gulp.src(path.join(config.dist.root, '**/*'))
